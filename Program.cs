a7d45d4a3ca2d52d07f21132d7f11c00e126fb4c
AmorFatiBA <you@example.com>
2020-09-21 22:36:16 +0300

    modife

--- Program.cs
+++ Program.cs
@@ -2,17 +2,28 @@
 using System.Text;
 using System.Collections.Generic;
 
-public class Example
+public class LinkedListExample
 {
+    public static void Display1(LinkedList<string> words, string text)
+    {
+        Console.WriteLine(text);
+        foreach(string word in words)
+        {
+            Console.Write(word," ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
     public static void Main()
     {
         // Create the link list.
-        string[] words =
-            { "the", "fox", "jumps", "over", "the", "dog" };
+        string[] words = { "the", "fox", "jumps", "over", "the", "dog" };
+
         LinkedList<string> sentence = new LinkedList<string>(words);
+
         Display(sentence, "The linked list values:");
-        Console.WriteLine("sentence.Contains(\"jumps\") = {0}",
-            sentence.Contains("jumps"));
+
+        Console.WriteLine("sentence.Contains(\"jumps\") = {0}", sentence.Contains("jumps"));
 
         // Add the word 'today' to the beginning of the linked list.
         sentence.AddFirst("today");
